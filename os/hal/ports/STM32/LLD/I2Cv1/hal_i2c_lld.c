253a1b553f4cbb114adb94f5890ad7137432835e
Andrew Tridgell <andrew@tridgell.net>
2019-01-12 16:38:21 +1100

    I2Cv1: support STM31F1 without filter control on i2c

--- os/hal/ports/STM32/LLD/I2Cv1/hal_i2c_lld.c
+++ os/hal/ports/STM32/LLD/I2Cv1/hal_i2c_lld.c
@@ -217,6 +217,7 @@ static void i2c_lld_set_clock(I2CDriver *i2cp) {
  * @notapi
  */
 static void i2c_lld_set_filter(I2CDriver *i2cp) {
+#ifdef I2C_FLTR_ANOFF
   I2C_TypeDef *dp = i2cp->i2c;
   i2cdutycycle_t duty = i2cp->config->duty_cycle;
   uint8_t filter;
@@ -244,6 +245,7 @@ static void i2c_lld_set_filter(I2CDriver *i2cp) {
   }
 
   dp->FLTR = (I2C_FLTR_ANOFF) | (I2C_FLTR_DNF & filter);
+#endif // I2C_FLTR_ANOFF
 }
 
 /**
