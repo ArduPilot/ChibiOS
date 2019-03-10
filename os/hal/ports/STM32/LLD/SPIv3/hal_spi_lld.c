3fa7d7071dbabfb67c682c7c7fa346b161f8f6da
Andrew Tridgell <andrew@tridgell.net>
2019-03-10 20:24:55 +1100

    STM32: added dummy tx and rx pointers for SPIv3
    these are needed to ensure that one-way SPI transfers are DMA safe

--- os/hal/ports/STM32/LLD/SPIv3/hal_spi_lld.c
+++ os/hal/ports/STM32/LLD/SPIv3/hal_spi_lld.c
@@ -825,11 +825,11 @@ void spi_lld_ignore(SPIDriver *spip, size_t n) {
 #endif
 #if defined(STM32_SPI_BDMA_REQUIRED)
   {
-    bdmaStreamSetMemory(spip->rx.bdma, &dummyrx);
+	bdmaStreamSetMemory(spip->rx.bdma, spip->config->dummyrx?spip->config->dummyrx:&dummyrx);
     bdmaStreamSetTransactionSize(spip->rx.bdma, n);
     bdmaStreamSetMode(spip->rx.bdma, spip->rxdmamode);
 
-    bdmaStreamSetMemory(spip->tx.bdma, &dummytx);
+    bdmaStreamSetMemory(spip->tx.bdma, spip->config->dummytx?spip->config->dummytx:&dummytx);
     bdmaStreamSetTransactionSize(spip->tx.bdma, n);
     bdmaStreamSetMode(spip->tx.bdma, spip->txdmamode);
 
@@ -842,11 +842,11 @@ void spi_lld_ignore(SPIDriver *spip, size_t n) {
 #endif
 #if defined(STM32_SPI_DMA_REQUIRED)
   {
-    dmaStreamSetMemory0(spip->rx.dma, &dummyrx);
+    dmaStreamSetMemory0(spip->rx.dma, spip->config->dummyrx?spip->config->dummyrx:&dummyrx);
     dmaStreamSetTransactionSize(spip->rx.dma, n);
     dmaStreamSetMode(spip->rx.dma, spip->rxdmamode);
 
-    dmaStreamSetMemory0(spip->tx.dma, &dummytx);
+    dmaStreamSetMemory0(spip->tx.dma, spip->config->dummytx?spip->config->dummytx:&dummytx);
     dmaStreamSetTransactionSize(spip->tx.dma, n);
     dmaStreamSetMode(spip->tx.dma, spip->txdmamode);
 
@@ -938,7 +938,7 @@ void spi_lld_send(SPIDriver *spip, size_t n, const void *txbuf) {
 #endif
 #if defined(STM32_SPI_BDMA_REQUIRED)
   {
-    bdmaStreamSetMemory(spip->rx.bdma, &dummyrx);
+    bdmaStreamSetMemory(spip->rx.bdma, spip->config->dummyrx?spip->config->dummyrx:&dummyrx);
     bdmaStreamSetTransactionSize(spip->rx.bdma, n);
     bdmaStreamSetMode(spip->rx.bdma, spip->rxdmamode);
 
@@ -955,7 +955,7 @@ void spi_lld_send(SPIDriver *spip, size_t n, const void *txbuf) {
 #endif
 #if defined(STM32_SPI_DMA_REQUIRED)
   {
-    dmaStreamSetMemory0(spip->rx.dma, &dummyrx);
+	dmaStreamSetMemory0(spip->rx.dma, spip->config->dummyrx?spip->config->dummyrx:&dummyrx);
     dmaStreamSetTransactionSize(spip->rx.dma, n);
     dmaStreamSetMode(spip->rx.dma, spip->rxdmamode);
 
@@ -997,7 +997,7 @@ void spi_lld_receive(SPIDriver *spip, size_t n, void *rxbuf) {
     bdmaStreamSetTransactionSize(spip->rx.bdma, n);
     bdmaStreamSetMode(spip->rx.bdma, spip->rxdmamode | STM32_BDMA_CR_MINC);
 
-    bdmaStreamSetMemory(spip->tx.bdma, &dummytx);
+    bdmaStreamSetMemory(spip->tx.bdma, spip->config->dummytx?spip->config->dummytx:&dummytx);
     bdmaStreamSetTransactionSize(spip->tx.bdma, n);
     bdmaStreamSetMode(spip->tx.bdma, spip->txdmamode);
 
@@ -1014,7 +1014,7 @@ void spi_lld_receive(SPIDriver *spip, size_t n, void *rxbuf) {
     dmaStreamSetTransactionSize(spip->rx.dma, n);
     dmaStreamSetMode(spip->rx.dma, spip->rxdmamode | STM32_DMA_CR_MINC);
 
-    dmaStreamSetMemory0(spip->tx.dma, &dummytx);
+    dmaStreamSetMemory0(spip->tx.dma, spip->config->dummytx?spip->config->dummytx:&dummytx);
     dmaStreamSetTransactionSize(spip->tx.dma, n);
     dmaStreamSetMode(spip->tx.dma, spip->txdmamode);
 
