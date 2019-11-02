6d1ff85a85979f8ef24735eadb4446935728020f
gdisirio <gdisirio@27425a3e-05d8-49a3-a47f-9c15f0e5edd8>
2019-11-02 07:17:53 +0000

    Fixed wrong paths in NIL4.
    git-svn-id: http://svn.osdn.net/svnroot/chibios/trunk@13150 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/nil/nil.mk
+++ os/nil/nil.mk
@@ -14,6 +14,16 @@ endif
 CHCONF := $(strip $(shell cat $(CHCONFDIR)/chconf.h | egrep -e "\#define"))
 
 KERNSRC := ${CHIBIOS}/os/nil/src/ch.c
+ifneq ($(findstring CH_CFG_USE_EVENTS TRUE,$(CHCONF)),)
+KERNSRC += $(CHIBIOS)/os/nil/src/chevt.c
+endif
+ifneq ($(findstring CH_CFG_USE_MESSAGES TRUE,$(CHCONF)),)
+KERNSRC += $(CHIBIOS)/os/nil/src/chmsg.c
+endif
+ifneq ($(findstring CH_CFG_USE_SEMAPHORES TRUE,$(CHCONF)),)
+KERNSRC += $(CHIBIOS)/os/nil/src/chsem.c
+endif
+
 else
 KERNSRC := ${CHIBIOS}/os/nil/src/ch.c
 endif
