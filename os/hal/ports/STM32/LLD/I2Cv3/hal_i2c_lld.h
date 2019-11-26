3e1b4dfa5602048821b33580d7780e9379fa62ab
Andrew Tridgell <andrew@tridgell.net>
2019-11-27 06:24:55 +1100

    I2C: fixed build without ISR limit

--- os/hal/ports/STM32/LLD/I2Cv3/hal_i2c_lld.h
+++ os/hal/ports/STM32/LLD/I2Cv3/hal_i2c_lld.h
@@ -557,12 +557,12 @@ struct hal_i2c_driver {
    * @brief limit of interrupts for this transfer
    */
   uint32_t                  isr_limit;
+#endif
 
   /**
      true when we are in an I2C transaction
    */
   bool                      in_transaction;
-#endif
 };
 
 /*===========================================================================*/
