1227e6f536fb375ab7550e34a48c944a97cb06f0
Andrew Tridgell <andrew@tridgell.net>
2021-09-10 19:39:58 +1000

    USARTv3: added IRQ callback hook

--- os/hal/ports/STM32/LLD/USARTv3/hal_serial_lld.h
+++ os/hal/ports/STM32/LLD/USARTv3/hal_serial_lld.h
@@ -500,6 +500,13 @@
 /* Driver data structures and types.                                         */
 /*===========================================================================*/
 
+typedef void (*uartirq_t)(void* sd);
+
+#define _serial_irq_code(sd) {                                      \
+  if ((sd)->config->irq_cb != NULL)                                 \
+    (sd)->config->irq_cb((sd)->config->ctx);                        \
+}
+
 /**
  * @brief   STM32 Serial Driver configuration structure.
  * @details An instance of this structure must be passed to @p sdStart()
@@ -526,6 +533,14 @@ typedef struct hal_serial_config {
    * @brief Initialization value for the CR3 register.
    */
   uint32_t                  cr3;
+  /**
+   * @brief Set callback from irq
+   */
+  uartirq_t                 irq_cb;
+  /**
+   * @pointer to ctx
+   */
+  void*                     ctx;
 } SerialConfig;
 
 /**
@@ -543,7 +558,9 @@ typedef struct hal_serial_config {
   /* Pointer to the USART registers block.*/                                \
   USART_TypeDef             *usart;                                         \
   /* Mask to be applied on received frames.*/                               \
-  uint8_t                   rxmask;
+  uint8_t                   rxmask;                                         \
+  /* config from start call.*/                                              \
+  const SerialConfig        *config;
 
 /*===========================================================================*/
 /* Driver macros.                                                            */
