71076d8e1324d1149ac841339919cd1c63c9dfa7
Andrew Tridgell <andrew@tridgell.net>
2021-10-11 18:28:44 +1100

    STM32: add STM32L431 support

--- os/hal/ports/STM32/STM32L4xx/hal_lld.h
+++ os/hal/ports/STM32/STM32L4xx/hal_lld.h
@@ -26,7 +26,7 @@
  *          - STM32_HSE_BYPASS (optionally).
  *          .
  *          One of the following macros must also be defined:
- *          - STM32L432xx, STM32L433xx, STM32L443xx.
+ *          - STM32L431xx, STM32L432xx, STM32L433xx, STM32L443xx.
  *          - STM32L471xx, STM32L475xx, STM32L476xx, STM32L496xx.
  *          - STM32L485xx, STM32L486xx, STM32L4A6xx.
  *          .
@@ -48,7 +48,7 @@
  * @name    Platform identification
  * @{
  */
-#if defined(STM32L432xx) || defined(STM32L433xx) || defined(STM32L443xx) || \
+#if defined(STM32L431xx) || defined(STM32L432xx) || defined(STM32L433xx) || defined(STM32L443xx) || \
     defined(STM32L452xx) || defined(STM32L471xx) || defined(STM32L475xx) || \
     defined(STM32L476xx) || defined(STM32L496xx) || defined(__DOXYGEN__)
 #define PLATFORM_NAME           "STM32L4xx Ultra Low Power"
@@ -777,6 +777,10 @@
 
 /* Only some devices have strongly checked mcuconf.h files. Others will be
    added gradually.*/
+#if defined(STM32L431xx) && !defined(STM32L431_MCUCONF)
+#error "Using a wrong mcuconf.h file, STM32L431_MCUCONF not defined"
+#endif
+
 #if defined(STM32L432xx) && !defined(STM32L432_MCUCONF)
 #error "Using a wrong mcuconf.h file, STM32L432_MCUCONF not defined"
 #endif
