e725d9b33376d133acd2d8e7632c6eee1918ec93
Andrew Tridgell <andrew@tridgell.net>
2022-11-09 07:15:31 +1100

    USARTv2: enable idle callback

--- os/hal/ports/STM32/LLD/USARTv2/hal_serial_lld.h
+++ os/hal/ports/STM32/LLD/USARTv2/hal_serial_lld.h
@@ -562,7 +562,10 @@ typedef struct hal_serial_config {
   /* Clock frequency for the associated USART/UART.*/                       \
   uint32_t                  clock;                                          \
   /* Mask to be applied on received frames.*/                               \
-  uint8_t                   rxmask;
+  uint8_t                   rxmask;                                         \
+  /* config from start call.*/                                              \
+  const SerialConfig        *config;
+
 
 /*===========================================================================*/
 /* Driver macros.                                                            */
