alex31/chibios_svn_mirror

Fixed bug #1242.
git-svn-id: http://svn.osdn.net/svnroot/chibios/branches/stable_20.3.x@15843 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

os/hal/ports/STM32/STM32L4xx/stm32_registry.h
@@ -1070,7 +1070,7 @@
 #if defined(STM32L496xx) || defined(STM32L4A6xx)
 
 /* Clock attributes.*/
-#define STM32_CLOCK_HAS_HSI48               FALSE
+#define STM32_CLOCK_HAS_HSI48               TRUE
 
 /* ADC attributes.*/
 #define STM32_HAS_ADC1                      TRUE

ArduPilot/ChibiOS

git-svn-id: https://svn.osdn.net/svnroot/chibios/branches/stable_21.11.x@15843 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

os/hal/ports/STM32/STM32L4xx/stm32_registry.h
@@ -1676,7 +1676,7 @@
 
 /* RCC attributes.*/
 #define STM32_RCC_HAS_HSI16                 TRUE
-#define STM32_RCC_HAS_HSI48                 FALSE
+#define STM32_RCC_HAS_HSI48                 TRUE
 #define STM32_RCC_HAS_MSI                   TRUE
 #define STM32_RCC_HAS_LSI                   TRUE
 #define STM32_RCC_HAS_LSI_PRESCALER         FALSE