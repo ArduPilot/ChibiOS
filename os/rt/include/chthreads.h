888e0654a85c3f2831aedc661891e2f9d5b5c58b
Andy Piper <github@andypiper.com>
2023-02-02 14:19:31 +0000

    rt: fix compilation warnings in kernel

--- os/rt/include/chthreads.h
+++ os/rt/include/chthreads.h
@@ -491,6 +491,8 @@ static inline bool chThdQueueIsEmptyI(threads_queue_t *tqp) {
  *
  * @iclass
  */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-align"
 static inline void chThdDoDequeueNextI(threads_queue_t *tqp, msg_t msg) {
   thread_t *tp;
 
@@ -503,6 +505,7 @@ static inline void chThdDoDequeueNextI(threads_queue_t *tqp, msg_t msg) {
   tp->u.rdymsg = msg;
   (void) chSchReadyI(tp);
 }
+#pragma GCC diagnostic pop
 
 #endif /* CHTHREADS_H */
 
