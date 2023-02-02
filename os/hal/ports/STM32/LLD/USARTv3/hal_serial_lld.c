79327f2a447c4898195f442e7aa44d98fd8bc60b
Andy Piper <github@andypiper.com>
2023-02-02 14:03:14 +0000

    USARTv3: support IRQ callbacks

--- os/hal/ports/STM32/LLD/USARTv3/hal_serial_lld.c
+++ os/hal/ports/STM32/LLD/USARTv3/hal_serial_lld.c
@@ -153,7 +153,9 @@ static const SerialConfig default_config =
   SERIAL_DEFAULT_BITRATE,
   0,
   USART_CR2_STOP1_BITS,
-  0
+  0,
+  NULL,
+  NULL
 };
 
 #if STM32_SERIAL_USE_USART1 || defined(__DOXYGEN__)
@@ -259,6 +261,8 @@ static void usart_init(SerialDriver *sdp,
                        const SerialConfig *config) {
   uint32_t brr, clock;
   USART_TypeDef *u = sdp->usart;
+
+  sdp->config = config;
 
   /* Baud rate setting.*/
   clock = sdp->clock;
@@ -1042,6 +1046,9 @@ void sd_lld_serve_interrupt(SerialDriver *sdp) {
 
   /* Writing CR1 once.*/
   u->CR1 = cr1;
+  if (isr & USART_ISR_IDLE) {
+      _serial_irq_code(sdp);
+  }
 }
 
 #endif /* HAL_USE_SERIAL */
