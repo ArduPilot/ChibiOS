c26d1990ac8fe4ef293bd7d75d16ad72c0927f30
Andy Piper <github@andypiper.com>
2023-02-02 12:37:46 +0000

    hal: support eicu driver

--- os/hal/hal.mk
+++ os/hal/hal.mk
@@ -49,6 +49,9 @@ endif
 ifneq ($(findstring HAL_USE_ICU TRUE,$(HALCONF)),)
 HALSRC += $(CHIBIOS)/os/hal/src/hal_icu.c
 endif
+ifneq ($(findstring HAL_USE_EICU TRUE,$(HALCONF)),)
+HALSRC += $(CHIBIOS)/os/hal/src/hal_eicu.c
+endif
 ifneq ($(findstring HAL_USE_MAC TRUE,$(HALCONF)),)
 HALSRC += $(CHIBIOS)/os/hal/src/hal_mac.c
 endif
@@ -109,6 +112,7 @@ HALSRC = $(CHIBIOS)/os/hal/src/hal.c \
          $(CHIBIOS)/os/hal/src/hal_i2c.c \
          $(CHIBIOS)/os/hal/src/hal_i2s.c \
          $(CHIBIOS)/os/hal/src/hal_icu.c \
+         $(CHIBIOS)/os/hal/src/hal_eicu.c \
          $(CHIBIOS)/os/hal/src/hal_mac.c \
          $(CHIBIOS)/os/hal/src/hal_mmc_spi.c \
          $(CHIBIOS)/os/hal/src/hal_pal.c \
--- os/hal/include/hal.h
+++ os/hal/include/hal.h
@@ -177,6 +177,10 @@
 #define HAL_USE_ICU                         FALSE
 #endif
 
+#if !defined(HAL_USE_EICU)
+#define HAL_USE_EICU                        FALSE
+#endif
+
 #if !defined(HAL_USE_MAC)
 #define HAL_USE_MAC                         FALSE
 #endif
@@ -325,6 +329,7 @@ static inline bool halClockSwitchMode(const halclkcfg_t *ccp) {
 #include "hal_i2c.h"
 #include "hal_i2s.h"
 #include "hal_icu.h"
+#include "hal_eicu.h"
 #include "hal_mac.h"
 #include "hal_pwm.h"
 #include "hal_rtc.h"
