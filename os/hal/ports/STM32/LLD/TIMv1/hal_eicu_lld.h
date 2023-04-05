9c7e423b92fe6957bbc239074509465b35512e00
Andy Piper <github@andypiper.com>
2023-04-05 18:31:07 +0200

    TIMv1: enable timers when using EICU

--- os/hal/ports/STM32/LLD/TIMv1/hal_eicu_lld.h
+++ os/hal/ports/STM32/LLD/TIMv1/hal_eicu_lld.h
@@ -295,6 +295,103 @@
 #error "EICU driver activated but no TIM peripheral assigned"
 #endif
 
+/* Checks on allocation of TIMx units.*/
+#if STM32_EICU_USE_TIM1
+#if defined(STM32_TIM1_IS_USED)
+#error "EICUD1 requires TIM1 but the timer is already used"
+#else
+#define STM32_TIM1_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM2
+#if defined(STM32_TIM2_IS_USED)
+#error "EICUD2 requires TIM2 but the timer is already used"
+#else
+#define STM32_TIM2_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM3
+#if defined(STM32_TIM3_IS_USED)
+#error "EICUD3 requires TIM3 but the timer is already used"
+#else
+#define STM32_TIM3_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM4
+#if defined(STM32_TIM4_IS_USED)
+#error "EICUD4 requires TIM4 but the timer is already used"
+#else
+#define STM32_TIM4_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM5
+#if defined(STM32_TIM5_IS_USED)
+#error "EICUD5 requires TIM5 but the timer is already used"
+#else
+#define STM32_TIM5_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM8
+#if defined(STM32_TIM8_IS_USED)
+#error "EICUD8 requires TIM8 but the timer is already used"
+#else
+#define STM32_TIM8_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM9
+#if defined(STM32_TIM9_IS_USED)
+#error "EICUD9 requires TIM9 but the timer is already used"
+#else
+#define STM32_TIM9_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM10
+#if defined(STM32_TIM10_IS_USED)
+#error "EICUD10 requires TIM10 but the timer is already used"
+#else
+#define STM32_TIM10_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM11
+#if defined(STM32_TIM11_IS_USED)
+#error "EICUD11 requires TIM11 but the timer is already used"
+#else
+#define STM32_TIM11_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM12
+#if defined(STM32_TIM12_IS_USED)
+#error "EICUD12 requires TIM12 but the timer is already used"
+#else
+#define STM32_TIM12_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM13
+#if defined(STM32_TIM13_IS_USED)
+#error "EICUD13 requires TIM13 but the timer is already used"
+#else
+#define STM32_TIM13_IS_USED
+#endif
+#endif
+
+#if STM32_EICU_USE_TIM14
+#if defined(STM32_TIM14_IS_USED)
+#error "EICUD14 requires TIM14 but the timer is already used"
+#else
+#define STM32_TIM14_IS_USED
+#endif
+#endif
+
 #if STM32_EICU_USE_TIM1 &&                                                    \
     !OSAL_IRQ_IS_VALID_PRIORITY(STM32_EICU_TIM1_IRQ_PRIORITY)
 #error "Invalid IRQ priority assigned to TIM1"
