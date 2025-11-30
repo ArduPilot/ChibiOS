ad84357cf7bf220aec9e033e23e997c58be1a01b
Thomas Watson <twatson52@icloud.com>
2025-11-30 10:48:04 -0600

    SDIOv1: use unsigned literals to match upstream

--- os/hal/ports/STM32/LLD/SDIOv1/hal_sdc_lld.c
+++ os/hal/ports/STM32/LLD/SDIOv1/hal_sdc_lld.c
@@ -286,21 +286,21 @@ static bool sdc_lld_wait_transaction_end(SDCDriver *sdcp, uint32_t n,
     osalThreadSuspendS(&sdcp->thread);
   }
 
-  /* Mask has now been set to zero by interrupt handler. */
-  osalSysUnlock();
+  /* Stopping operations, waiting for transfer completion at DMA level, then
+     the stream is disabled and cleared.*/
+  dmaWaitCompletion(sdcp->dma);
+  sdcp->sdio->MASK  = 0U;
+  sdcp->sdio->DCTRL = 0U;
 
-  /* Data transfer not complete, let error cleanup stop DMA.*/
   if ((sdcp->sdio->STA & SDIO_STA_DATAEND) == 0U) {
+    osalSysUnlock();
     return HAL_FAILED;
   }
 
-  /* Waiting for transfer completion at DMA level, then the stream is disabled
-     and cleared.*/
-  dmaWaitCompletion(sdcp->dma);
-  sdcp->sdio->DCTRL = 0U;
-
   /* Clearing status.*/
   sdcp->sdio->ICR = SDIO_ICR_ALL_FLAGS;
+
+  osalSysUnlock();
 
   /* Finalize transaction.*/
   if (n > 1U)
